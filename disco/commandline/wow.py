0c17c8ae268c4822265e5f207fd30e25268dfc92
Kevin Dalton <kmdalton@pop-os.localdomain>
2022-08-24 13:44:07 -0400

    add option to save csv file from wow

--- disco/commandline/wow.py
+++ disco/commandline/wow.py
@@ -12,6 +12,7 @@ def main():
     parser = ArgumentParser("Generate random laue patterns from pdb data")
     parser.add_argument("-n", default=100, type=int, help="Number of patterns to draw")
     parser.add_argument("-s", default=None, type=str, help='Save movie of your `wow` session to an mp4 file')
+    parser.add_argument("--csv", default=None, type=str, help="Save the coordinates of spots to a csv file.")
     parser = parser.parse_args()
 
     prefix = '/'.join(abspath(__file__).split('/')[:-1])
@@ -29,6 +30,7 @@ def main():
     from celluloid import Camera
     cam = Camera(f)
     lmin,lmax = 1., 1.2 #<-- a typical wavelength range in Å
+    dfout = []
     for i in range(parser.n):
         datum = data.sample()
         try:
@@ -52,6 +54,13 @@ def main():
         plt.xlim(0., 2*beam_x)
         plt.ylim(0., 2*beam_y)
         cam.snap()
+        if parser.csv is not None:
+            df = pd.DataFrame({
+                'x' : x,
+                'y' : y,
+            })
+            df['frame'] = i
+            dfout.append(df)
 
     anim = cam.animate()
     if parser.s is not None:
@@ -60,4 +69,7 @@ def main():
             print("HIHIHII")
         elif parser.s.endswith(".mp4"):
             anim.save(parser.s)
+    if parser.csv is not None:
+        dfout = pd.concat(dfout)
+        dfout.to_csv(parser.csv)
     plt.show()
