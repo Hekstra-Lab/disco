adf5ae6ddc0df49d9ce1dc29eede52e26a75fa65
Kevin Dalton <kmdalton@pop-os.localdomain>
2022-09-01 11:20:49 -0400

    add various cli options

--- disco/commandline/wow.py
+++ disco/commandline/wow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from disco import Ball,Detector
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import gemmi
@@ -12,6 +13,11 @@ def main():
     parser = ArgumentParser("Generate random laue patterns from pdb data")
     parser.add_argument("-n", default=100, type=int, help="Number of patterns to draw")
     parser.add_argument("-s", default=None, type=str, help='Save movie of your `wow` session to an mp4 file')
+    parser.add_argument("--cell", nargs=6, default=None, type=float, help="Six floats corresponding to [a b c alpha beta gamma].")
+    parser.add_argument("--dmin", default=None, type=float, help="Resolution cutoff in Angstroms.")
+    parser.add_argument("--spacegroup", default=None, type=str, help="Space group.")
+    parser.add_argument("--csv", default=None, type=str, help="Save the coordinates of spots to a csv file.")
+
     parser = parser.parse_args()
 
     prefix = '/'.join(abspath(__file__).split('/')[:-1])
@@ -29,29 +35,44 @@ def main():
     from celluloid import Camera
     cam = Camera(f)
     lmin,lmax = 1., 1.2 #<-- a typical wavelength range in Å
+    dfout = []
     for i in range(parser.n):
-        datum = data.sample()
-        try:
-            cell = gemmi.UnitCell(
-                datum['Length a (Å)'],
-                datum['Length b (Å)'],
-                datum['Length c (Å)'],
-                datum['Angle alpha (°)'],
-                datum['Angle beta (°)'],
-                datum['Angle gamma (°)'],
-            )
-            sg = gemmi.SpaceGroup(str(datum['Space Group'].iloc[0]))
-            dmin = datum['High Resolution Limit'].to_numpy('float32')
-            ball = Ball(cell, sg, dmin, lmin, lmax)
-            s1 = ball.get_random_scattered_beam_wavevectors()
-        except:
-            continue
+        if parser.spacegroup is not None:
+            sg = gemmi.SpaceGroup(parser.spacegroup)
+            cell = gemmi.UnitCell(*parser.cell)
+            dmin = parser.dmin
+        else:
+            datum = data.sample()
+            try:
+                cell = gemmi.UnitCell(
+                    datum['Length a (Å)'],
+                    datum['Length b (Å)'],
+                    datum['Length c (Å)'],
+                    datum['Angle alpha (°)'],
+                    datum['Angle beta (°)'],
+                    datum['Angle gamma (°)'],
+                )
+                sg = gemmi.SpaceGroup(str(datum['Space Group'].iloc[0]))
+                dmin = datum['High Resolution Limit'].to_numpy('float32')
+            except:
+                continue
+        ball = Ball(cell, sg, dmin, lmin, lmax)
+        s1 = ball.get_random_scattered_beam_wavevectors()
+        wavelength = np.reciprocal(np.sqrt((s1*s1).sum(-1)))
         x, y = detector.project(s1)
         plt.plot(x, y, 'k.', alpha=0.3)
         plt.plot(beam_x, beam_y, 'xr')
         plt.xlim(0., 2*beam_x)
         plt.ylim(0., 2*beam_y)
         cam.snap()
+        if parser.csv is not None:
+            df = pd.DataFrame({
+                'x' : x,
+                'y' : y,
+                'wavelength' : wavelength,
+            })
+            df['frame'] = i
+            dfout.append(df)
 
     anim = cam.animate()
     if parser.s is not None:
@@ -60,4 +81,7 @@ def main():
             print("HIHIHII")
         elif parser.s.endswith(".mp4"):
             anim.save(parser.s)
+    if parser.csv is not None:
+        dfout = pd.concat(dfout)
+        dfout.to_csv(parser.csv)
     plt.show()
